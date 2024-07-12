eadf6b82eaa4aad796fd6c9d8c41152c9f0fc171
zedy <zedy@wicresoft.com>
2024-07-12 11:00:17 +0800

    add azure cli in devcontainer configuration

--- infra/hooks/postprovision.sh
+++ infra/hooks/postprovision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [$OPENAI_HOST == "azure"]; then
-    AZURE_OPENAI_API_KEY=$(az cognitiveservices account keys list --name $AZURE_OPEN_SERVICE --resource-group $AZURE_RESOURCE_GROUP --query key1 --output tsv)
+if [ $OPENAI_HOST == "azure" ]; then
+    AZURE_OPENAI_API_KEY=$(az cognitiveservices account keys list --name $AZURE_OPENAI_SERVICE --resource-group $AZURE_RESOURCE_GROUP --query key1 --output tsv)
     azd env set AZURE_OPENAI_API_KEY $AZURE_OPENAI_API_KEY
 fi
